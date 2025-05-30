6ca7f69e72a2a6ae62c692fce59048152f801560
VietCH57 <viet.ch57@gmail.com>
2025-05-30 22:26:53 +0700

    Test_AI

--- alpha_zero_lite/__init__.py
+++
@@ -1,6 +0,0 @@
-# Import main components to make them accessible via the package
-from alpha_zero_lite.network import AlphaZeroLiteNetwork, ResBlock
-from alpha_zero_lite.ai import AlphaZeroLite
-from alpha_zero_lite.trainer import AlphaZeroLiteTrainer
-from alpha_zero_lite.dataset import SelfPlayDataset, load_training_data
-from alpha_zero_lite.headless_game import HeadlessChessGame
